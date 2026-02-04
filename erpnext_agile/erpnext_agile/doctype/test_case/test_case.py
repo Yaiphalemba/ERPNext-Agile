e550649d047668d58a45471584c1fda08cd117e2
Yaiphalemba <mangshatabams@rapifuzz.com>
2026-02-04 12:15:15 +0530

    feat: owner and assignees feature added in Test Case doc

--- erpnext_agile/erpnext_agile/doctype/test_case/test_case.py
+++ erpnext_agile/erpnext_agile/doctype/test_case/test_case.py
@@ -2,7 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.desk.form.assign_to import add, clear
 
 class TestCase(Document):
     def autoname(self):
@@ -29,6 +31,7 @@ class TestCase(Document):
     def validate(self):
         """Validate test case"""
         # Ensure at least one test step
+        frappe.msgprint("validate called")
         if not self.test_steps:
             frappe.throw("At least one test step is required")
         
@@ -38,6 +41,12 @@ class TestCase(Document):
         
         # Validate linked items
         self.validate_linked_items()
+        
+    def on_update(self):
+        # Track assignee changes
+        frappe.msgprint("on_update called")
+        if self.has_value_changed("assigned_to_users"):
+            self.track_assignee_changes()
     
     def validate_linked_items(self):
         """Validate linked tasks/projects"""
@@ -86,3 +95,27 @@ class TestCase(Document):
         new_case.status = "Draft"
         new_case.insert()
         return new_case.name
+    
+    def track_assignee_changes(self):
+        """Track changes to assignees"""
+        old_doc = self.get_doc_before_save()
+        if not old_doc:
+            return
+        
+        old_assignees = set([d.user for d in old_doc.get("assigned_to_users", [])])
+        new_assignees = set([d.user for d in self.get("assigned_to_users", [])])
+        
+        added = new_assignees - old_assignees
+        removed = old_assignees - new_assignees
+        
+        if added:
+            user_id = list(added)
+            add({
+                    "assign_to": user_id,
+                    "doctype": "Test Case",
+                    "name": self.name,
+                    "description": self.title
+                })
+        
+        if removed:
+            clear("Test Case", self.name)
